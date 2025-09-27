1bee03d2e3e8e56f0106708260ca7621a1d740e9
SY19870627 <105897408+SY19870627@users.noreply.github.com>
2025-09-27 12:52:42 +0800

    fix: align core utilities with ts55 strictness

--- src/core/DataRepo.ts
+++ src/core/DataRepo.ts
@@ -1,6 +1,8 @@
 export class DataRepo {
   private cache = new Map<string, any>();
-  constructor(private loader:(path:string)=>Promise<any>){
+  private loader:(path:string)=>Promise<any>;
+  constructor(loader:(path:string)=>Promise<any>){
+    this.loader = loader;
   }
   async get<T=any>(name:string): Promise<T> {
     if (this.cache.has(name)) return this.cache.get(name);
